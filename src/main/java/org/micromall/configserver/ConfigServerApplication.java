7f53f8e6dc70dee98bc07251260aa4f95c04d47f
Marouane Dbibih <marrouane.dbibih@gmail.com>
2024-12-01 21:24:39 +0100

    Setup profiles (local,k8s) & config the configuration from github repository

--- src/main/java/org/micromall/configserver/ConfigServerApplication.java
+++ src/main/java/org/micromall/configserver/ConfigServerApplication.java
@@ -1,4 +1,5 @@
 package org.micromall.configserver;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
